85e497f5e68e12e05b19016b93a914f1d895bc8f
rodrigoprobst <becker.rodrigo@outlook.com>
2019-03-26 22:04:58 -0300

    desafio feito

---
+++ 1/desafio/src/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('src/views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+
+app.set('view engine', 'njk')
+
+const checkMiddleware = (req, res, next) => {
+  const { age } = req.query
+  if (!age) {
+    return res.redirect('/')
+  } else {
+    return next()
+  }
+}
+
+app.get('/', (req, res) => {
+  return res.render('home')
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+  if (age < 18) {
+    return res.redirect(`/minor?age=${age}`)
+  } else if (age >= 18) {
+    return res.redirect(`/major?age=${age}`)
+  }
+})
+
+app.use(checkMiddleware)
+
+app.get('/major', (req, res) => {
+  return res.render('afterCheck', { maiormenor: 'maior', age: req.query.age })
+})
+
+app.get('/minor', (req, res) => {
+  return res.render('afterCheck', { maiormenor: 'menor', age: req.query.age })
+})
+
+app.listen(3000)
