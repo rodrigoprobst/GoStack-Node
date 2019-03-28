3286141838be5d7151bd3fa57cbda1949d47593b
rodrigoprobst <becker.rodrigo@outlook.com>
2019-03-28 19:08:42 -0300

    Autenticação

--- aulas/modulo2/src/routes/index.js
+++ aulas/modulo2/src/routes/index.js
@@ -6,6 +6,13 @@ const routes = express.Router()
 
 const Controllers = require('../app/controllers')
 
+routes.get('/app/dashboard', (req, res) => {
+  return res.render('dashboard')
+})
+
+routes.get('/', Controllers.SessionController.create)
+routes.post('/signin', Controllers.SessionController.store)
+
 routes.get('/signup', Controllers.UserController.create)
 routes.post(
   '/signup',
