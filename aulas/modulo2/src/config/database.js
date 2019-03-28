636f63caac5bb6f85703a0936e4c0478fe2dd6f1
rodrigoprobst <becker.rodrigo@outlook.com>
2019-03-27 22:01:57 -0300

    Model & Migration Usuário

--- aulas/modulo2/src/config/database.js
+++ aulas/modulo2/src/config/database.js
@@ -1,29 +1,13 @@
 module.exports = {
-  development: {
-    dialect: 'postgres',
-    host: '127.0.0.1',
-    username: 'docker',
-    password: 'docker',
-    database: 'gonodemodulo2',
-    operatorAliases: false,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true
-    }
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'postgres'
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
   }
 }
