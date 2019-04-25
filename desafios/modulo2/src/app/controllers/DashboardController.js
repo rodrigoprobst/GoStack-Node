869c589eb0834beb657110e4eb6f6d62174768a9
rodrigoprobst <becker.rodrigo@outlook.com>
2019-04-24 22:40:22 -0300

    desafio 2 e circleCI

--- desafios/modulo2/src/app/controllers/DashboardController.js
+++ desafios/modulo2/src/app/controllers/DashboardController.js
@@ -2,6 +2,12 @@ const { User } = require('../models')
 
 class DashboardController {
   async index (req, res) {
+    if (req.session.user && req.session.user.provider) {
+      const appointments = await User.findAll({
+        where: { provider: true }
+      })
+      return res.render('dashboard-provider')
+    }
     const providers = await User.findAll({
       where: { provider: true }
     })
